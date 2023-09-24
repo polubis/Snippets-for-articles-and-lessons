1165b901632af610796df66b128ab9fc73facb37
Adrian Połubiński <polubis@wp.pl>
2023-09-24 17:54:49 +0200

    Create gatsby-config-gtag-change.ts

---
+++ Gatsby5, Tailwind, jest, Cypress, TypeScript - modern ecosystem course/gatsby-config-gtag-change.ts
@@ -0,0 +1,12 @@
+"gatsby-plugin-postcss",
+{
+  resolve: "gatsby-plugin-google-gtag",
+  options: {
+    trackingIds: [
+      "GA-TRACKING_ID", // Google Analytics.
+    ],
+    head: false,
+    anonymize: true,
+  },
+},
+"gatsby-plugin-image",
