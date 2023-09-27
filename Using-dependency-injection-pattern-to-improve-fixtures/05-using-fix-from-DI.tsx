dfa1adbd58e5fabca909303b763c119a6ee358a0
Adrian Połubiński <polubis@wp.pl>
2023-09-27 09:54:56 +0200

    Create 05-using-fix-from-DI.tsx

---
+++ Using-dependency-injection-pattern-to-improve-fixtures/05-using-fix-from-DI.tsx
@@ -0,0 +1,9 @@
+// 🧪 Sort function that works!
+import { sort } from 'sort-which-works-ok';
+
+const UsersView = () => {
+  return (
+    // Now it will work 💚.
+    <UsersList sortFn={sort} />
+  );
+};
