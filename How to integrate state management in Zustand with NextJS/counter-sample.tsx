89e40c16e511e7c7622b0da9d39cc0267b37aadb
Adrian Połubiński <polubis@wp.pl>
2023-09-28 12:22:10 +0200

    Create counter-sample.tsx

---
+++ How to integrate state management in Zustand with NextJS/counter-sample.tsx
@@ -0,0 +1,36 @@
+// @@@ Inside use-counter-store.ts @@@.
+export interface CounterStore {
+  counter: number;
+}
+
+export const useCounterStore = create<CounterStore>(() => ({
+  counter: 0,
+}));
+
+// @@@ Inside counter page @@@.
+
+interface CounterPageProps {
+  counter: number;
+}
+
+export const getStaticProps: GetStaticProps<CounterPageProps> = async () => {
+  return {
+    props: {
+      counter: 3,
+    },
+  };
+};
+
+const CounterPage = ({ counter }: CounterPageProps) => {
+  // Counter returned from server on first render.
+  console.log(counter); // 3.
+
+  const counterStore = useCounter();
+
+  // Client "counterStore" value on first render.
+  console.log(counterStore.counter); // 0.
+
+  return <div>Any JSX content</div>;
+};
+
+export default CounterPage;
