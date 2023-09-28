706e63d95622762fbee20006038c7de18e2db257
Adrian Połubiński <polubis@wp.pl>
2023-09-28 12:48:42 +0200

    Update counter-al-final.tsx

--- How to integrate state management in Zustand with NextJS/counter-al-final.tsx
+++ How to integrate state management in Zustand with NextJS/counter-al-final.tsx
@@ -1,6 +1,6 @@
 const CounterPage = ({ counter }: CounterPageProps) => {
   console.log(counter); // 3.
-  const counterStore = useStoreSync(useCounterStore, { counter });
+  const counterStore = useStoreSync(useCounterStore, { counter })();
   console.log(counterStore.counter); // 3.
 
   return <div>{counterStore.counter}</div>; // It's 3.
