87477dd6ed4a1293b996449282be6beeeca6a7d7
Adrian Połubiński <polubis@wp.pl>
2023-09-28 11:48:19 +0200

    Create use-server-store.ts

---
+++ How to integrate state management in Zustand with NextJS/use-server-store.ts
@@ -0,0 +1,27 @@
+// The "isClient" is just check for window !== undefined.
+import { isClient } from '@system/utils';
+import { useMemo, useRef } from 'react';
+import { create, type StoreApi, type UseBoundStore } from 'zustand';
+
+const useStoreSync = <T>(
+  useStore: UseBoundStore<StoreApi<T>>,
+  state: T
+): UseBoundStore<StoreApi<T>> => {
+  // Ref to store flag and avoid rerender.
+  const unsynced = useRef(true);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // Creating default store with initial state.
+  const useServerStore = useMemo(() => create<T>(() => state), []);
+
+  if (unsynced.current) {
+    // Setting state and changing flag.
+    useStore.setState(state);
+    unsynced.current = false;
+  }
+  // For "client" we'll return original store.
+  // For "server" the initial one passed from server
+  // props.
+  return isClient() ? useStore : useServerStore;
+};
+
+export { useStoreSync };
