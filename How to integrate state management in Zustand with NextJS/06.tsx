f6fd528a8d87e56dc0c4da50728a84f6e14daef9
Adrian Połubiński <polubis@wp.pl>
2023-09-28 12:58:21 +0200

    Update 06.tsx

--- How to integrate state management in Zustand with NextJS/06.tsx
+++ How to integrate state management in Zustand with NextJS/06.tsx
@@ -1,2 +1,2 @@
-  // Original client hook and state to set before first render.
-  const counterStore = useStoreSync(useCounterStore, { counter });
+// Original client hook and state to set before first render.
+const counterStore = useStoreSync(useCounterStore, { counter });
