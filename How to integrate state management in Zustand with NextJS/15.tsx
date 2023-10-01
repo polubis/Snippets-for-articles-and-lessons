650be4efdbc1337a97c56ff8c0feb487588c4e1b
Adrian Połubiński <polubis@wp.pl>
2023-10-01 11:20:25 +0200

    Create 15.tsx

---
+++ How to integrate state management in Zustand with NextJS/15.tsx
@@ -0,0 +1,7 @@
+const CounterPage = ({ counter }: CounterPageProps) => {
+  console.log(counter) // 3.
+  const counterStore = useStoreSync(useCounterStore, { counter })()
+  console.log(counterStore.counter) // 3.
+
+  return <div>{counterStore.counter}</div> // It's 3.
+}
