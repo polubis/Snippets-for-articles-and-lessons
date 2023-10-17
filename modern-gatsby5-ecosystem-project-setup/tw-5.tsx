c9ccec40291e751ada4fcfbbf3d3c2e460c05fae
Adrian Połubiński <polubis@wp.pl>
2023-10-17 12:23:05 +0200

    Create tw-5.tsx

---
+++ modern-gatsby5-ecosystem-project-setup/tw-5.tsx
@@ -0,0 +1,7 @@
+// Inside index.tsx file - the home page.
+<main>
+  <h1 className="text-3xl top-5 font-bold underline">Hello!</h1>
+  <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+    Badge
+  </span>
+</main>
