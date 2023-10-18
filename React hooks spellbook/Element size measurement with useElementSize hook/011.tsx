ab7ab847fd392436e8161feade30843cae8639a1
Adrian Połubiński <polubis@wp.pl>
2023-10-18 11:03:38 +0200

    Create 011.tsx

---
+++ React hooks spellbook/Element size measurement with useElementSize hook/011.tsx
@@ -0,0 +1,60 @@
+import { Subject, throttleTime } from 'rxjs';
+
+const useElementSize = <T extends HTMLElement>(
+  config?: ElementSizeConfig
+): ElementSizeReturn<T> => {
+  const [state, setState] = useState<ElementSizeState>({
+    status: 'undetected',
+  });
+  const ref = useRef<T | null>(null);
+  const observerRef = useRef<ResizeObserver | null>(null);
+
+  // Creating event emitter and object to observe only once
+  // per hook usage.
+  const { changed, changed$ } = useMemo(() => {
+    const changed = new Subject<ElementSizeState>();
+    const changed$ = changed.asObservable();
+    return { changed, changed$ };
+  }, []);
+
+  useEffect(() => {
+    // Listening for events and applying a throttling
+    // mechanism with a given delay.
+    const sub = changed$.pipe(throttleTime(config?.delay ?? 150)).subscribe({
+      next: (size) => {
+        setState(size);
+      },
+    });
+
+    return () => {
+      // Removes subscription.
+      sub.unsubscribe();
+    };
+  }, [changed$, config?.delay]);
+
+  useIsomorphicLayoutEffect(() => {
+    const observeElement = () => {
+      const target = ref?.current ?? document.body;
+
+      const { width, height } = target.getBoundingClientRect();
+
+      setState({ status: 'detected', height, width });
+
+      observerRef.current = new ResizeObserver((entries) => {
+        const { width, height } = entries[0].contentRect;
+        // Pushing event with payload to the stream.
+        changed.next({ status: 'detected', height, width });
+      });
+
+      observerRef.current.observe(target);
+    };
+
+    observeElement();
+
+    return () => {
+      observerRef.current?.disconnect();
+    };
+  }, [changed]);
+
+  return [state, ref];
+};
