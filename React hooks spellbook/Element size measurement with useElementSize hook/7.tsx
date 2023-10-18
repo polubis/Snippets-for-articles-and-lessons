466e7a89b433802b57bffe18b7c63dee5ff6e14a
Adrian Połubiński <polubis@wp.pl>
2023-10-18 10:41:07 +0200

    Update 7.tsx

--- React hooks spellbook/Element size measurement with useElementSize hook/7.tsx
+++ React hooks spellbook/Element size measurement with useElementSize hook/7.tsx
@@ -8,9 +8,11 @@ import type {
 const useElementSize = <T extends HTMLElement>(
   config?: ElementSizeConfig
 ): ElementSizeReturn<T> => {
+  // Here we'll store the state with metadata.
   const [state, setState] = useState<ElementSizeState>({
     status: 'undetected',
   });
+  // The reference of the HTML element to be used by the consumer.
   const ref = useRef<T | null>(null);
 
   return [state, ref];
