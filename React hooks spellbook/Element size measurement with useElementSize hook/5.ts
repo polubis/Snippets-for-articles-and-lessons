20ed42ac2da4f422f593098eeecbb9089d67074d
Adrian Połubiński <polubis@wp.pl>
2023-10-18 09:58:29 +0200

    Create 5.ts

---
+++ React hooks spellbook/Element size measurement with useElementSize hook/5.ts
@@ -0,0 +1,47 @@
+import type { MutableRefObject } from 'react';
+
+// The basic object with metadata that we need.
+interface ElementSize {
+  width: number;
+  height: number;
+}
+
+// State shape which reflects situation
+// when we're not able to detect 
+// the size - server side rendering situation.
+interface UndetectedState {
+  status: 'undetected';
+}
+
+// State shape which reflects situation
+// when are able to detect - client side.
+interface DetectedState extends ElementSize {
+  status: 'detected';
+}
+// Union of states.
+type ElementSizeState = UndetectedState | DetectedState;
+// Helper type - may be needed in consumer components.
+type ElementSizeStateStatus = ElementSizeState['status'];
+
+// This will be passed to our hook.
+interface ElementSizeConfig {
+  delay?: number;
+}
+
+// This will be returned by our hook.
+// It's a readonly tuple [state, ref]. 
+// "MutableRefObject" is the object returned by React
+// "useRef" hook.
+type ElementSizeReturn<T extends HTMLElement> = Readonly<
+  [ElementSizeState, MutableRefObject<T | null>]
+>;
+
+export type {
+  UndetectedState,
+  ElementSize,
+  DetectedState,
+  ElementSizeStateStatus,
+  ElementSizeState,
+  ElementSizeReturn,
+  ElementSizeConfig,
+};
