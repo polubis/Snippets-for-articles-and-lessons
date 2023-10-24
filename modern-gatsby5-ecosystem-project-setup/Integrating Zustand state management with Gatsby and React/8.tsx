f262c2714583412dc2bcee4b53a82137297f3ed8
Adrian Połubiński <polubis@wp.pl>
2023-10-24 10:38:29 +0200

    Create 8.tsx

---
+++ modern-gatsby5-ecosystem-project-setup/Integrating Zustand state management with Gatsby and React/8.tsx
@@ -0,0 +1,15 @@
+const HomePage: React.FC = () => {
+  return (
+    <ThemeToggler>
+      {({ theme, toggleTheme }) => (
+        <main onClick={() => toggleTheme(theme === `dark` ? `light` : `dark`)}>
+          <h1 className="text-3xl top-5 font-bold underline">Hello!</h1>
+          <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+            Badge
+          </span>
+          <span className="dark:text-white text-black">Theme test</span>
+        </main>
+      )}
+    </ThemeToggler>
+  );
+};
