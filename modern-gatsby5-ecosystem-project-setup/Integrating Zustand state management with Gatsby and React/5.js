03a656712353448377135399ae89f47dddf37fbc
Adrian Połubiński <polubis@wp.pl>
2023-10-24 10:20:08 +0200

    Create 5.js

---
+++ modern-gatsby5-ecosystem-project-setup/Integrating Zustand state management with Gatsby and React/5.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [`./src/pages/*.{js,jsx,ts,tsx}`],
+  content: [
+    `./src/pages/*.{js,jsx,ts,tsx}`,
+    `./src/views/home/*.{js,jsx,ts,tsx}`,
+  ],
