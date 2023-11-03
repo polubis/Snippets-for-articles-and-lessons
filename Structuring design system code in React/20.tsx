f5f63f93ac71825bf8b8c29fc477d3e2af956113
Adrian Połubiński <polubis@wp.pl>
2023-11-03 14:11:48 +0100

    Create 20.tsx

---
+++ Structuring design system code in React/20.tsx
@@ -0,0 +1,25 @@
+<DesignSystemProvider>
+  <GlobalStyle />
+  <Calendar>
+    <Calendar.Item>
+      <Font variant={6} element="h6">
+        1
+      </Font>
+    </Calendar.Item>
+    <Calendar.Item>
+      <Font variant={6} element="h6">
+        2
+      </Font>
+    </Calendar.Item>
+    <Calendar.Item>
+      <Font variant={6} element="h6">
+        3
+      </Font>
+    </Calendar.Item>
+    <Calendar.Item active>
+      <Font variant={6} element="h6">
+        4
+      </Font>
+    </Calendar.Item>
+  </Calendar>
+</DesignSystemProvider>
