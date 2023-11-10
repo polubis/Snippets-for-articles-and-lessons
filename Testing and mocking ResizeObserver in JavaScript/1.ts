6b470292cb9eb558bd3ebd78381cc7c20cfd3c38
Adrian Połubiński <polubis@wp.pl>
2023-11-10 08:51:53 +0100

    Create 1.ts

---
+++ Testing and mocking ResizeObserver in JavaScript/1.ts
@@ -0,0 +1,23 @@
+// Inside useElementSize.ts hook.
+useLayoutEffect(() => {
+  const observeElement = () => {
+    const target = ref?.current ?? document.body;
+
+    const { width, height } = target.getBoundingClientRect();
+
+    setState({ status: 'detected', height, width });
+    // Here the testing environment will throw an error 💥!
+    observerRef.current = new ResizeObserver((entries) => {
+      const { width, height } = entries[0].contentRect;
+      changed.next({ status: 'detected', height, width });
+    });
+
+    observerRef.current.observe(target);
+  };
+
+  observeElement();
+
+  return () => {
+    observerRef.current?.disconnect();
+  };
+}, [changed]);
