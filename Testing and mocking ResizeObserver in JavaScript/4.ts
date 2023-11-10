ffaa3c8ddfc400bd94d91ca6548bf49e384cc385
Adrian Połubiński <polubis@wp.pl>
2023-11-10 09:10:54 +0100

    Update 4.ts

--- Testing and mocking ResizeObserver in JavaScript/4.ts
+++ Testing and mocking ResizeObserver in JavaScript/4.ts
@@ -1,12 +1,12 @@
-  // Previous code is above 🔼.
-  // We need any component to test our hook in an "integration" way.
-  const ComponentFixture = () => {
-      const [state, ref] = useElementSize<HTMLDivElement>();
-      return (
-        <div ref={ref}>
-          {state.status === 'detected'
-            ? `detected: width: ${state.width}, height: ${state.height}`
-            : state.status}
-        </div>
-      );
-  };
+// Previous code is above 🔼.
+// We need any component to test our hook in an "integration" way.
+const ComponentFixture = () => {
+    const [state, ref] = useElementSize<HTMLDivElement>();
+    return (
+      <div ref={ref}>
+        {state.status === 'detected'
+          ? `detected: width: ${state.width}, height: ${state.height}`
+          : state.status}
+      </div>
+    );
+};
