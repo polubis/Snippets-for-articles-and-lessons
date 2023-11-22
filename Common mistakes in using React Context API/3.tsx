aff07e29858f2d8c04878202b89f213b52703c83
Adrian Połubiński <polubis@wp.pl>
2023-11-22 13:08:49 +0100

    Create 3.tsx

---
+++ Common mistakes in using React Context API/3.tsx
@@ -0,0 +1,32 @@
+const Context = createContext({ state: false, setState: () => {} })
+
+const INeedYourState = () => {
+  const state = useContext(Context)
+  console.log("Thanks for your state!", state)
+  return null
+}
+
+const Component2 = () => {
+  return <Component3 />
+}
+
+const Component3 = () => {
+  return <Component4 />
+}
+
+const Component4 = () => {
+  return <INeedYourState />
+}
+
+const Component1 = () => {
+  return <Component2 />
+}
+const MyStateProvider = () => {
+  const [state, setState] = useState({ open: true })
+
+  return (
+    <Context.Provider value={{ state, setState }}>
+      <Component1 />
+    </Context.Provider>
+  )
+}
