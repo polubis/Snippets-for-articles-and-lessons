2b371e1dce8bb13c77f1c1f093f81da70840ddf9
Adrian Połubiński <polubis@wp.pl>
2023-11-22 12:46:26 +0100

    Create 1.tsx

---
+++ Common mistakes in using React Context API/1.tsx
@@ -0,0 +1,22 @@
+const INeedYourState = props => {
+  console.log("Thanks for your state!", props)
+  return null
+}
+
+const Component2 = props => {
+  return <Component3 {...props} />
+}
+
+const Component3 = props => {
+  return <Component4 {...props} />
+}
+
+const Component4 = props => {
+  return <INeedYourState {...props} />
+}
+
+const Component1 = () => {
+  const [state, setState] = useState({ open: true })
+
+  return <Component2 state={state} setState={setState} />
+}
