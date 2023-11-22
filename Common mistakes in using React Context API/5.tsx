ef6278206055f76af1a02a6cb579752785684f04
Adrian Połubiński <polubis@wp.pl>
2023-11-22 13:32:19 +0100

    Create 5.tsx

---
+++ Common mistakes in using React Context API/5.tsx
@@ -0,0 +1,25 @@
+// CounterProvider.tsx file.
+import { useState } from "react"
+
+const CounterContext = createContext(0)
+
+const CounterProvider = () => {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <CounterContext.Provider value={counter}>
+      {/* Any component that is using Counter.tsx inside... */}
+    </CounterContext.Provider>
+  )
+}
+
+// Counter.tsx component.
+import { CounterContext } from "./CounterProvider"
+import { useContext } from "react"
+
+const Counter = () => {
+  // 💢 This is really bad...
+  const counter = useContext(CounterContext)
+
+  return <div>Counter is: {counter}</div>
+}
