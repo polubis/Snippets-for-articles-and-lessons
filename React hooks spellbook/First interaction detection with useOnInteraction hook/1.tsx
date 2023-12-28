eacf5d5db5e96b9322f79ab5fade4e5760967112
Adrian Połubiński <polubis@wp.pl>
2023-12-28 11:55:28 +0100

    Create 1.tsx

---
+++ React hooks spellbook/First interaction detection with useOnInteraction hook/1.tsx
@@ -0,0 +1,6 @@
+// Adding events.
+window.addEventListener(`mousemove`, listener);
+window.addEventListener(`touchstart`, listener);
+// Removing events.  
+window.removeEventListener(`touchstart`, listener);  
+window.removeEventListener(`mousemove`, listener);
